3f656007bb1173ab9c78691d37c658ab43c3e576
alejandrojcastaneira <alejandrocastaneira1991@gmail.com>
2020-04-19 20:13:51 +0200

    define project structure

---
+++ trainings/fged/en_flair_train_on_wiki-fbt_eval_on_1k-wfb-g.py
@@ -0,0 +1,81 @@
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import TokenEmbeddings, StackedEmbeddings, FlairEmbeddings, BytePairEmbeddings
+from typing import List
+import flair
+import numpy as np
+import random
+import torch
+from definitions import ROOT_DIR
+from datetime import datetime
+
+manualSeed = 1
+
+np.random.seed(manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+torch.cuda.manual_seed(manualSeed)
+torch.cuda.manual_seed_all(manualSeed)
+
+torch.backends.cudnn.deterministic = True
+
+print(flair.device)
+
+data_folder = ROOT_DIR + '/datasets/HAnDS/'
+
+columns = {0: 'text', 1: 'ner'}
+
+corpus: Corpus = ColumnCorpus(data_folder, columns, train_file='fed_wiki-fbt_train_downsampled.txt',
+                                                   test_file='fed_1k-wfb-g_test.txt',
+                                                   dev_file='fed_1k-wfb-g_dev.txt')
+print(len(corpus.train))
+print(len(corpus.test))
+print(len(corpus.dev))
+print(corpus.obtain_statistics())
+
+tag_type = 'ner'
+
+tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+
+print(tag_dictionary)
+
+embedding_types: List[TokenEmbeddings] = [
+
+    FlairEmbeddings("en-forward-fast", fine_tune=True),
+
+    BytePairEmbeddings(language='en', dim=300, syllables=300000),
+
+    FlairEmbeddings("en-backward-fast", fine_tune=True),
+]
+
+embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+from flair.models import SequenceTagger
+
+tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                        embeddings=embeddings,
+                                        tag_dictionary=tag_dictionary,
+                                        tag_type=tag_type, use_crf=True)
+
+# 6. initialize trainer
+from flair.trainers import ModelTrainer
+from datetime import datetime
+
+now = datetime.now()
+
+dt_string = now.strftime("%Y-%m-%d_%H:%M:%S")
+
+trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+
+# 7. start training
+trainer.train( ROOT_DIR + '/models/fged/en_flair+bpemb_emb_'+ dt_string,
+              learning_rate=0.1,
+              mini_batch_size=208,
+              max_epochs=25, patience=3, checkpoint=True, embeddings_storage_mode='none', num_workers=12, use_amp=True,)
+
+# 8. plot training curves (optional)
+from flair.visual.training_curves import Plotter
+plotter = Plotter()
+plotter.plot_training_curves(ROOT_DIR + '/models/fged/en_flair+bpemb_emb_'+ dt_string+'/loss.tsv')
+plotter.plot_weights(ROOT_DIR + '/models/fged/en_flair+bpemb_emb_'+ dt_string+'/weights.txt')
